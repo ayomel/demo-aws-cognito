a64d4b4ff1e62a7f15c89d2df39dcc4852d8e5df
Melverton Hunter <mel@Melvertons-Air.lan>
2020-10-29 15:58:24 -0400

    fixup: Minor stuff

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,61 +5,78 @@ import UserPool from '../UserPool'
 import '../App.css'
 
 function Login() {
-	const [username, setUsername] = useState('')
-	const [email, setEmail] = useState('')
-	const [password, setPassword] = useState('')
+  const [username, setUsername] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
 
-	const onSubmit = (e) => {
-		e.preventDefault()
+  const signOut = (e) => {
+    e.preventDefault()
 
-		const user = new CognitoUser({
-			Username: email,
-			Pool: UserPool,
+    const user = new CognitoUser({
+      Username: email,
+      Pool: UserPool,
 		})
 
-		const AuthDetails = new AuthenticationDetails({
-			Username: email,
-			Password: password,
-		})
+    //regular sign out
+		user.signOut()
 
-		user.authenticateUser(AuthDetails, {
-			onSuccess: (data) => {
-				console.log('Success:', data)
-			},
-			onFailure: (data) => {
-				console.log('Failed:', data)
-			},
-			newPasswordRequired: (data) => {
-				console.log('Password:', data)
-			},
-		})
-	}
+    // Global signout for an authenticated user(invalidates all issued tokens).
+    // user.globalSignOut((result) => console.log(result))
+  }
 
-	return (
-		<div className='App'>
-			<form onSubmit={onSubmit}>
-				<input
-					type='username'
-					placeholder='Username'
-					value={username}
-					onChange={(e) => setUsername(e.target.value)}
-				/>
-				<input
-					type='email'
-					placeholder='Email'
-					value={email}
-					onChange={(e) => setEmail(e.target.value)}
-				/>
-				<input
-					type='text'
-					placeholder='Password'
-					value={password}
-					onChange={(e) => setPassword(e.target.value)}
-				/>
-				<button type='submit'>Login</button>
-			</form>
-		</div>
-	)
+  const onSubmit = (e) => {
+    e.preventDefault()
+
+    const user = new CognitoUser({
+      Username: email,
+      Pool: UserPool,
+    })
+
+    const AuthDetails = new AuthenticationDetails({
+      Username: email,
+      Password: password,
+    })
+
+    user.authenticateUser(AuthDetails, {
+      onSuccess: (data) => {
+        console.log('Success:', data)
+      },
+      onFailure: (data) => {
+        console.log('Failed:', data)
+      },
+      newPasswordRequired: (data) => {
+        console.log('Password:', data)
+      },
+    })
+  }
+
+  return (
+    <div className='App'>
+      <form onSubmit={onSubmit}>
+        <input
+          type='username'
+          placeholder='Username'
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type='email'
+          placeholder='Email'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type='text'
+          placeholder='Password'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type='submit'>Login</button>
+      </form>
+
+      <button onClick={signOut}>Sign Out</button>
+    </div>
+  )
 }
 
 export default Login
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,61 +1,87 @@
 import React, { useState } from 'react'
+import { CognitoUser } from 'amazon-cognito-identity-js'
+
 import UserPool from '../UserPool'
 import '../App.css'
 
 function SignUp() {
-	const [username, setUsername] = useState('')
-	const [email, setEmail] = useState('')
-	const [password, setPassword] = useState('')
+  const [username, setUsername] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [confirmNumber, setConfirmNumber] = useState('')
 
-	const attributeList = [
-		{
-			Name: 'phone_number',
-			Value: '+15555555555',
-		},
-		{
-			Name: 'email',
-			Value: email,
-		},
-	]
+  const attributeList = [
+    {
+      Name: 'phone_number',
+      Value: '+15555555555',
+    },
+    {
+      Name: 'email',
+      Value: email,
+    },
+  ]
 
-	const onSubmit = (e) => {
-		e.preventDefault()
+  const onSubmit = (e) => {
+    e.preventDefault()
 
-		// UserPool.signUp(email, password, attributeList, (err, data) => {
-		// 	// if (err) console.error(err)
-		// 	console.log(data, err)
-		// })
-		UserPool.signUp(email, password, attributeList, null, (err, data) => {
-			// if (err) console.error(err)
-			console.log(data, err)
-		})
-	}
+    UserPool.signUp(email, password, attributeList, null, (err, data) => {
+      if (err) console.error(err)
+      console.log(data, err)
+    })
+  }
 
-	return (
-		<div className='App'>
-			<form onSubmit={onSubmit}>
-				<input
-					type='username'
-					placeholder='Username'
-					value={username}
-					onChange={(e) => setUsername(e.target.value)}
-				/>
-				<input
-					type='email'
-					placeholder='Email'
-					value={email}
-					onChange={(e) => setEmail(e.target.value)}
-				/>
-				<input
-					type='text'
-					placeholder='Password'
-					value={password}
-					onChange={(e) => setPassword(e.target.value)}
-				/>
-				<button type='submit'>SignUp</button>
-			</form>
-		</div>
-	)
+  const ConfirmSubmit = (e) => {
+    e.preventDefault()
+
+    const user = new CognitoUser({
+      Username: email,
+      Pool: UserPool,
+    })
+
+    user.confirmRegistration(confirmNumber, true, function (err, result) {
+      if (err) {
+        console.log(err.message || JSON.stringify(err))
+        return
+      }
+      console.log('call result: ' + result)
+    })
+  }
+
+  return (
+    <div className='App'>
+      <form onSubmit={onSubmit}>
+        <input
+          type='username'
+          placeholder='Username'
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type='email'
+          placeholder='Email'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type='text'
+          placeholder='Password'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type='submit'>SignUp</button>
+      </form>
+
+      <form onSubmit={ConfirmSubmit}>
+        <input
+          type='text'
+          placeholder='ConfirmNumber'
+          value={confirmNumber}
+          onChange={(e) => setConfirmNumber(e.target.value)}
+        />
+        <button type='submit'>Confirm Number</button>
+      </form>
+    </div>
+  )
 }
 
 export default SignUp
